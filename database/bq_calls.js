176fa2fd6116a49659d4934876b08a285382adc7
HSaldivar <hector.saldivar@metricamovil.com>
2025-12-02 14:21:01 -0600

    Fix function environment

--- database/bq_calls.js
+++ database/bq_calls.js
@@ -9,7 +9,7 @@ const bigQuery = new BigQuery({
 async function sendData(data){
     try {
         let chuckSize = parseInt(process.env.CHUNCK_SIZE || 50);
-        let query = 'CALL ' + process.env.BQ_DATASET + '.vehicle_mdvr_data_qa(@data)';
+        let query = 'CALL ' + process.env.BQ_DATASET + '.vehicle_mdvr_data(@data)';
         let results = [];
         for (let i = 0; i < data.length; i+=chuckSize) {
             console.log('ENVIANDO PAQUETE DE DATOS: ' + (i/chuckSize + 1) + '/' + Math.ceil(data.length/chuckSize));
